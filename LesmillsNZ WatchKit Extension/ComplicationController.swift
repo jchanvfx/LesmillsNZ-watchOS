0dfbbe4545bac865677f3108c858ddbae30a855e
jchanvfx <jjohnnycchan@gmail.com>
2021-03-03 23:50:49 +1300

    rearrage modules

--- LesmillsNZ WatchKit Extension/ComplicationController.swift
+++ LesmillsNZ WatchKit Extension/ComplicationController.swift
@@ -14,7 +14,11 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
 
     func getComplicationDescriptors(handler: @escaping ([CLKComplicationDescriptor]) -> Void) {
         let descriptors = [
-            CLKComplicationDescriptor(identifier: "complication", displayName: "LesmillsNZ", supportedFamilies: CLKComplicationFamily.allCases)
+            CLKComplicationDescriptor(
+                identifier: "complication",
+                displayName: "Lesmills NZ",
+                supportedFamilies: CLKComplicationFamily.allCases
+            )
             // Multiple complication support can be added here with more descriptors
         ]
         
