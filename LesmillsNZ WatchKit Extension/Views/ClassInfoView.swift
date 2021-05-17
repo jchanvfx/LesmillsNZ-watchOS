9d4963d1fe9b70dadd4134f49ce2ca4bcd6e2cf3
jchanvfx <jjohnnycchan@gmail.com>
2021-05-17 12:39:34 +1200

    info view state check

--- LesmillsNZ WatchKit Extension/Views/ClassInfoView.swift
+++ LesmillsNZ WatchKit Extension/Views/ClassInfoView.swift
@@ -8,30 +8,54 @@
 import SwiftUI
 
 struct ClassInfoView: View {
+    @Environment(\.scenePhase) var scenePhase
+    @State private var textColor = "#ffffff"
     @State private var iconName = "stopwatch"
     @State private var iconColor = "#25f578"
+    @State private var classColor = "#636363"
+    @State private var hasStarted = false
 
     let classInfo: FitnessClass
-    
+
+    private func updateClassState() {
+        self.hasStarted = classInfo.hasStarted
+        if !classInfo.hasStarted {
+            self.textColor = "#ffffff"
+            self.iconName = "stopwatch"
+            self.iconColor = "#25f578"
+            self.classColor = classInfo.colorText
+        } else {
+            self.textColor = "#636363"
+            self.iconName = "flag.slash"
+            self.iconColor = "#484848"
+            self.classColor = "#636363"
+        }
+    }
+
     var body: some View {
         VStack {
             Text(classInfo.nameText)
                 .font(.system(size: 14))
                 .fontWeight(.semibold)
+                .foregroundColor(Color(hex: textColor))
             Text("with")
                 .font(.system(size: 10))
                 .foregroundColor(.gray)
             Text(classInfo.instructorsText)
                 .font(.system(size: 12))
+                .foregroundColor(Color(hex: textColor))
             RoundedRectangle(cornerRadius: 3)
-                .fill(Color(hex: classInfo.colorText))
+                .fill(Color(hex: classColor))
                 .frame(height: 2)
             Text(classInfo.timeText)
                 .font(.system(.caption))
+                .strikethrough(hasStarted)
             Text(classInfo.locationText)
                 .font(.system(.caption))
+                .foregroundColor(Color(hex: textColor))
             Text("\(classInfo.dayText) \(classInfo.dateText) \(classInfo.monthText)")
                 .font(.system(.caption))
+                .foregroundColor(Color(hex: textColor))
             Image(systemName: iconName)
                 .foregroundColor(Color(hex: iconColor))
                 .padding(.top, 2)
@@ -41,9 +65,14 @@ struct ClassInfoView: View {
         }
         .navigationTitle("Info")
         .onAppear{
-            if classInfo.hasStarted {
-                self.iconName = "flag.slash"
-                self.iconColor = "#484848"
+            DispatchQueue.main.async {
+                self.updateClassState()
+            }
+        }
+        .onChange(of: scenePhase) { newPhase in
+            if newPhase != .active { return }
+            DispatchQueue.main.async {
+                self.updateClassState()
             }
         }
     }
